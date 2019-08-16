02933f450b0f5f16e76626ffed834a3b2cfd7e23
Rigoberto Torres <rigoberto.torres@gl.local>
2019-08-16 10:46:36 -0400

    levantamiento de proyecto

---
+++ src/components/Pregunta.js
@@ -0,0 +1,49 @@
+import React, { Fragment, useState } from 'react';
+import Error from './Error'
+
+const Pregunta = (props) => {
+
+    const { guardarPresupuesto, guardarPreguntaPresupuesto, guardarRestante } = props
+    // definir el state
+
+    const [cantidad, guardarCantidad] = useState(0)
+    const [error, guardarError] = useState(false)
+
+    const agregarPresupuesto = e => {
+        e.preventDefault();
+        // validar
+        if(cantidad < 1 || isNaN(cantidad)) {
+            guardarError(true)
+            return;
+        }
+
+        // si se pasa la validación
+        guardarError(false)
+        guardarPresupuesto(cantidad)
+        guardarRestante(cantidad)
+        guardarPreguntaPresupuesto(false)
+    }
+
+    return (
+        <Fragment>
+            <h2>Coloca tu presupuesto</h2>
+            { error ? <Error mensaje="El presupuesto es incorrecto" /> : null }
+            <form
+                onSubmit={agregarPresupuesto}
+            >
+                <input type="number"
+                    className="u-full-width"
+                    placeholder="Agrega tu presupuesto"
+                    onChange={e => guardarCantidad(parseInt(e.target.value, 10))}
+                />
+                <input
+                    type="submit"
+                    className="button-primary u-full-width"
+                    value="Definir Presupuesto"
+                />
+            </form>
+        </Fragment>
+    );
+};
+
+export default Pregunta;
